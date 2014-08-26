6349815f965e3767b0967a45071bf5c3c1214c80
Andres Ruiz <ipruch@gmail.com>
2014-08-26 20:28:59 +0200

    Tabulation formatting

--- include/X-GS/Integrator.hpp
+++ include/X-GS/Integrator.hpp
@@ -9,33 +9,31 @@
 namespace xgs {
     
 	// Needed struct for RK4 method.
-    struct Derivative
-    {
-        sf::Vector2f dp; // dp/dt = velocity
-        sf::Vector2f dv; // dv/dt = acceleration
-    };
+	struct Derivative
+	{
+		sf::Vector2f dp; // dp/dt = velocity
+		sf::Vector2f dv; // dv/dt = acceleration
+	};
     
 	
 	// Integrator class. Implements Euler and RK4 integration methods.
-    class Integrator
-    {
+	class Integrator
+	{
 		// Class methods
-    public:
-        void static                     Euler(PhysicState& physics,
-											  sf::Transformable& transf,
-                                              const HiResDuration& dt);
-        void static                     RK4(PhysicState& physics,
-											sf::Transformable& transf,
-                                            const HiResDuration& dt);
+	public:
+		void static                     Euler(PhysicState& physics,
+							sf::Transformable& transf,
+							const HiResDuration& dt);
+		void static                     RK4(PhysicState& physics,
+							sf::Transformable& transf,
+							const HiResDuration& dt);
         
-        
-    private:
-        Derivative static               evaluateRK4(const PhysicState& initialPhysics,
-													const sf::Transformable& initialTransf,
-                                                    const HiResDuration& dt,
-                                                    const Derivative& d);
-        sf::Vector2f static             accelerationRK4(const PhysicState&
-														physics);
+	private:
+		Derivative static               evaluateRK4(const PhysicState& initialPhysics,
+							const sf::Transformable& initialTransf,
+                                                    	const HiResDuration& dt,
+                                                    	const Derivative& d);
+		sf::Vector2f static             accelerationRK4(const PhysicState& physics);
     };
     
 } // namespace xgs
