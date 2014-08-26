07e8f5e22a91679ed39031cf53ed53eeb16faf18
Andres Ruiz <ipruch@gmail.com>
2014-08-26 20:48:38 +0200

    Tabulation formatting

--- include/X-GS/Time.hpp
+++ include/X-GS/Time.hpp
@@ -9,9 +9,9 @@
 namespace xgs {
     
     // std::chrono typedefs
-    typedef std::chrono::time_point<std::chrono::high_resolution_clock> HiResTime;
-    typedef std::chrono::nanoseconds HiResDuration;
-    typedef std::chrono::high_resolution_clock HiResClock;
+    typedef std::chrono::time_point<std::chrono::high_resolution_clock>     HiResTime;
+    typedef std::chrono::nanoseconds                                        HiResDuration;
+    typedef std::chrono::high_resolution_clock                              HiResClock;
     
 } // namespace xgs
 
