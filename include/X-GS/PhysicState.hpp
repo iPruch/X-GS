f6d5a4325f2698bafbc989ceb6149c6ee8b33a03
Andres Ruiz <ipruch@gmail.com>
2014-08-26 20:30:55 +0200

    Tabulation formatting

--- include/X-GS/PhysicState.hpp
+++ include/X-GS/PhysicState.hpp
@@ -5,26 +5,26 @@
 
 namespace xgs {
 	
-    // Needed struct for entities updated with the Integrator class.
-    class PhysicState
-    {
+	// Needed struct for entities updated with the Integrator class.
+	class PhysicState
+	{
 		// Methods
 	public:
-		PhysicState();			// Constructor
-		void					setVelocity(sf::Vector2f velocity);
-		void					setForce(sf::Vector2f force);
-		void					setMass(float mass);
+						PhysicState();
+		void				setVelocity(sf::Vector2f velocity);
+		void				setForce(sf::Vector2f force);
+		void				setMass(float mass);
 		
 		const sf::Vector2f		getVelocity() const;
 		const sf::Vector2f		getForce() const;
-		const float				getMass() const;
+		const float			getMass() const;
 		
 		// Variables (member / properties)
 	private:
-        //sf::Vector2f position; // Get position from transform
-        sf::Vector2f			mVelocity;
-        sf::Vector2f			mForce;
-        float					mMass;
+		//sf::Vector2f position; // Get position from transform
+		sf::Vector2f			mVelocity;
+		sf::Vector2f			mForce;
+		float				mMass;
     };
 	
 } // namespace xgs
